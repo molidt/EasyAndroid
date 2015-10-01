1fca6b96a2212b571dd236191822273ea278cf12
Jianan <qinxiandiqi@foxmail.com>
2015-10-01 23:17:51 +0800

    update html TextView

--- easyandroid-widget/src/main/java/com/qinxiandiqi/easyandroid/widget/HtmlTextView.java
+++ easyandroid-widget/src/main/java/com/qinxiandiqi/easyandroid/widget/HtmlTextView.java
@@ -16,8 +16,13 @@
 package com.qinxiandiqi.easyandroid.widget;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.Html;
 import android.util.AttributeSet;
 import android.widget.TextView;
+
+import org.xml.sax.XMLReader;
 
 /**
  * Created by Jianan on 9/29/15.
@@ -34,5 +39,19 @@ public class HtmlTextView extends TextView{
 
     public HtmlTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public void setHtml(String strHtml){
+        Html.fromHtml(strHtml, new Html.ImageGetter() {
+            @Override
+            public Drawable getDrawable(String source) {
+                return null;
+            }
+        }, new Html.TagHandler() {
+            @Override
+            public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+
+            }
+        });
     }
 }
