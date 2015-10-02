d82039102e20854dc82d17cbee91ef37a8fb2682
Jianan <qinxiandiqi@foxmail.com>
2015-10-02 23:39:22 +0800

    update html TextView

--- easyandroid-widget/src/main/java/com/qinxiandiqi/easyandroid/widget/HtmlTextView.java
+++ easyandroid-widget/src/main/java/com/qinxiandiqi/easyandroid/widget/HtmlTextView.java
@@ -19,7 +19,12 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.text.Editable;
 import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.TextView;
 
 import org.xml.sax.XMLReader;
@@ -28,6 +33,8 @@ import org.xml.sax.XMLReader;
  * Created by Jianan on 9/29/15.
  */
 public class HtmlTextView extends TextView{
+
+    private HtmlClickListener mListener;
 
     public HtmlTextView(Context context) {
         super(context);
@@ -41,17 +48,59 @@ public class HtmlTextView extends TextView{
         super(context, attrs, defStyleAttr);
     }
 
+    public void setHtml(int res){
+        initHtml(getContext().getString(res));
+    }
+
     public void setHtml(String strHtml){
-        Html.fromHtml(strHtml, new Html.ImageGetter() {
+        initHtml(strHtml);
+    }
+
+    private void initHtml(String strHtml){
+        CharSequence charSequence = Html.fromHtml(strHtml, new Html.ImageGetter() {
             @Override
             public Drawable getDrawable(String source) {
-                return null;
+                return mListener.getDrawable(source);
             }
         }, new Html.TagHandler() {
             @Override
             public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
-
+                mListener.handleTag(opening, tag, output, xmlReader);
             }
         });
+
+        SpannableStringBuilder builder = new SpannableStringBuilder(charSequence);
+        URLSpan[] urlSpans = builder.getSpans(0, charSequence.length(), URLSpan.class);
+        for(URLSpan span : urlSpans){
+            int start = builder.getSpanStart(span);
+            int end = builder.getSpanEnd(span);
+            int flag = builder.getSpanFlags(span);
+            final String link = span.getURL();
+            builder.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(View widget) {
+                    mListener.onClickLink(link);
+                }
+            }, start, end, flag);
+            builder.removeSpan(span);
+        }
+        setText(charSequence);
+        setLinksClickable(true);
+        setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public static class HtmlClickListener{
+
+        public void onClickLink(String link){
+
+        }
+
+        public Drawable getDrawable(String source) {
+            return null;
+        }
+
+        public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+
+        }
     }
 }
