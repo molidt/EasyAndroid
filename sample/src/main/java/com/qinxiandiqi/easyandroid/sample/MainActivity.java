c23504f7756661d17c838e97e56248826daa3de8
Jianan <qinxiandiqi@foxmail.com>
2015-10-05 21:32:15 +0800

    add network and database modules

--- sample/src/main/java/com/qinxiandiqi/easyandroid/sample/MainActivity.java
+++ sample/src/main/java/com/qinxiandiqi/easyandroid/sample/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.qinxiandiqi.easyandroid.sample.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
