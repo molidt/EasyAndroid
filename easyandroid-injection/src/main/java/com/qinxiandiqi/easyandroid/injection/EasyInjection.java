8d30f48567153425bf66cea6dedf09170f1771a7
Jianan <qinxiandiqi@foxmail.com>
2016-06-17 21:52:19 +0800

    update EasyInject.

--- easyandroid-injection/src/main/java/com/qinxiandiqi/easyandroid/injection/EasyInjection.java
+++ easyandroid-injection/src/main/java/com/qinxiandiqi/easyandroid/injection/EasyInjection.java
@@ -20,6 +20,13 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -50,8 +57,36 @@ public class EasyInjection {
         this.holder = holder;
     }
 
-    public final <T extends View> T view(int... resID){
-        return findView(resID);
+    public final <T extends View> T view(int... viewIDs){
+        return findView(viewIDs);
+    }
+
+    public final TextView textView(int... viewIDs){
+        return (TextView) findView(viewIDs);
+    }
+
+    public final ImageView imageView(int... viewIDs){
+        return (ImageView) findView(viewIDs);
+    }
+
+    public final ListView listView(int... viewIDs){
+        return (ListView) findView(viewIDs);
+    }
+
+    public final Button button(int... viewIDs){
+        return (Button) findView(viewIDs);
+    }
+
+    public final LinearLayout linearLayout(int... viewIDs){
+        return (LinearLayout) findView(viewIDs);
+    }
+
+    public final RelativeLayout relativeLayout(int... viewIDs){
+        return (RelativeLayout) findView(viewIDs);
+    }
+
+    public final FrameLayout frameLayout(int... viewIDs){
+        return (FrameLayout) findView(viewIDs);
     }
 
     public final String string(int resID){
@@ -70,13 +105,13 @@ public class EasyInjection {
         return AnimationUtils.loadAnimation(rootView.getContext(), resID);
     }
 
-    public final EasyInjection onClick(View.OnClickListener listener, int... resIDs){
-        View tempView = view(resIDs);
+    public final EasyInjection onClick(View.OnClickListener listener, int... viewIDs){
+        View tempView = view(viewIDs);
         tempView.setOnClickListener(listener);
         return this;
     }
 
-    public final EasyInjection onClick(final Method target, int... resIDs){
+    public final EasyInjection onClick(final Method target, int... viewIDs){
         View.OnClickListener listener = new View.OnClickListener(){
 
             @Override
@@ -91,19 +126,29 @@ public class EasyInjection {
                 }
             }
         };
-        return onClick(listener, resIDs);
+        return onClick(listener, viewIDs);
     }
 
-    public final EasyInjection onClick(String methodName, int... resIDs){
+    public final EasyInjection onClick(String methodName, int... viewIDs){
         try {
             Method method = holder.getClass().getDeclaredMethod(methodName, View.class);
-            return onClick(method, resIDs);
+            return onClick(method, viewIDs);
         } catch (NoSuchMethodException e) {
             throw new RuntimeException("Can't find out " + methodName + " method.", e);
         }
     }
 
-    public final EasyInjection onItemClick(final Method target, int resID){
+    public final EasyInjection onItemClick(AdapterView.OnItemClickListener listener, int... viewIDs){
+        View tempView = findView(viewIDs);
+        if(tempView instanceof AdapterView){
+            ((AdapterView)tempView).setOnItemClickListener(listener);
+        }else{
+            throw new RuntimeException("The view with id(" + viewIDs + ") is not a AdapterView.");
+        }
+        return this;
+    }
+
+    public final EasyInjection onItemClick(final Method target, int... viewIDs){
         AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -117,25 +162,25 @@ public class EasyInjection {
                 }
             }
         };
-        View tempView = view(resID);
-        if(tempView instanceof AdapterView){
-            ((AdapterView)tempView).setOnItemClickListener(listener);
-        }else{
-            throw new RuntimeException("The view with id(" + resID + ") is not a AdapterView.");
-        }
-        return this;
+        return onItemClick(listener, viewIDs);
     }
 
-    public final EasyInjection onItemClick(String methodName, int resID){
+    public final EasyInjection onItemClick(String methodName, int... viewIDs){
         try {
             Method method = holder.getClass().getMethod(methodName, AdapterView.class, View.class, Integer.class, Long.class);
-            return onItemClick(method, resID);
+            return onItemClick(method, viewIDs);
         } catch (NoSuchMethodException e) {
             throw new RuntimeException("Can't find out " + methodName + " method.", e);
         }
     }
 
-    public final EasyInjection onLongClick(final Method target, int... resIDs){
+    public final EasyInjection onLongClick(View.OnLongClickListener listener, int... viewIDs){
+        View tempView = findView(viewIDs);
+        tempView.setOnLongClickListener(listener);
+        return this;
+    }
+
+    public final EasyInjection onLongClick(final Method target, int... viewIDs){
         View.OnLongClickListener listener = new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -150,15 +195,13 @@ public class EasyInjection {
                 return false;
             }
         };
-        View tempView = view(resIDs);
-        tempView.setOnLongClickListener(listener);
-        return this;
+        return onLongClick(listener, viewIDs);
     }
 
-    public final EasyInjection onLongClick(String methodName, int... resIDs){
+    public final EasyInjection onLongClick(String methodName, int... viewIDs){
         try {
             Method method = holder.getClass().getDeclaredMethod(methodName, View.class);
-            onLongClick(method, resIDs);
+            onLongClick(method, viewIDs);
         } catch (NoSuchMethodException e) {
             throw new RuntimeException("Can't find out method name - " + methodName, e);
         }
@@ -166,7 +209,17 @@ public class EasyInjection {
         return this;
     }
 
-    public final EasyInjection onItemLongClick(final Method target, int resID){
+    public final EasyInjection onItemLongClick(AdapterView.OnItemLongClickListener listener, int... viewIDs){
+        View tempView = findView(viewIDs);
+        if(tempView instanceof AdapterView){
+            ((AdapterView)tempView).setOnItemLongClickListener(listener);
+        }else{
+            throw new RuntimeException("The view with id(" + viewIDs + ") is not a AdapterView.");
+        }
+        return this;
+    }
+
+    public final EasyInjection onItemLongClick(final Method target, int... viewIDs){
         AdapterView.OnItemLongClickListener listener = new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -180,33 +233,33 @@ public class EasyInjection {
                 return false;
             }
         };
-        View tempView = view(resID);
-        if(tempView instanceof AdapterView){
-            ((AdapterView)tempView).setOnItemLongClickListener(listener);
-        }else{
-            throw new RuntimeException("The view with id(" + resID + ") is not a AdapterView.");
-        }
-        return this;
+        return onItemLongClick(listener, viewIDs);
     }
 
-    public final EasyInjection onItemLongClick(String methodName, int resID){
+    public final EasyInjection onItemLongClick(String methodName, int... viewIDs){
         try {
             Method method = holder.getClass().getDeclaredMethod(methodName, AdapterView.class, View.class, Integer.class, Long.class);
-            return onItemLongClick(method, resID);
+            return onItemLongClick(method, viewIDs);
         } catch (NoSuchMethodException e) {
-            throw new RuntimeException("can't find out method of this name:" + methodName, e);
+            throw new RuntimeException("EasyInject can't find out the method of this name:" + methodName, e);
         }
     }
 
-    private <T extends View> T findView(int... resIDs){
-        if(resIDs.length <= 0){
+    /**
+     * Find the view by view's id.
+     * @param viewIDs the view's ids,those ids should order from parent to child.
+     * @param <T> the view's type
+     * @return the target view.
+     */
+    private <T extends View> T findView(int... viewIDs){
+        if(viewIDs.length <= 0){
             throw new RuntimeException("EasyInjection don't know which view you want, please give me the view's id.");
         }
-        String strKey = getStrViewID(resIDs);
+        String strKey = getStrViewID(viewIDs);
         View view = cacheViews.get(strKey);
         if(view == null){
             view = rootView;
-            for (int resID : resIDs) {
+            for (int resID : viewIDs) {
                 view = view.findViewById(resID);
                 if (view == null) {
                     throw new RuntimeException("EasyInjection can't find out the view which id is " + strKey);
@@ -217,13 +270,18 @@ public class EasyInjection {
         return (T)view;
     }
 
-    private String getStrViewID(int... resIDs){
+    /**
+     * Get the string ID of resource.
+     * @param viewIDs the set of resource's id
+     * @return the string ID.
+     */
+    private String getStrViewID(int... viewIDs){
         cacheStrBuilder.delete(0, cacheStrBuilder.length());
-        for(int i = 0; i < resIDs.length; i ++){
+        for(int i = 0; i < viewIDs.length; i ++){
             if(cacheStrBuilder.length() > 0){
                 cacheStrBuilder.append("#");
             }
-            cacheStrBuilder.append(resIDs[i]);
+            cacheStrBuilder.append(viewIDs[i]);
         }
         return cacheStrBuilder.toString();
     }
